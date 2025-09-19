c8f133915ee8a6bf4cf5398c1cccc3682f11306f
SHKBRD <121646151+SHKBRD@users.noreply.github.com>
2025-09-18 22:24:37 -0400

    add energy toggle stuff

--- scenes/actors/plotdevelopments/energy/energy_gen.gd
+++ scenes/actors/plotdevelopments/energy/energy_gen.gd
@@ -1,8 +1,61 @@
 extends Development
 class_name EnergyGenDevelopment
 
+enum GenerationType {
+	NONE,
+	ENERGY,
+	ZAB
+}
+
+var generationType: GenerationType = GenerationType.ENERGY
+
 func apply_calculated_work_value() -> void:
-	pass
+	if generationType == GenerationType.ENERGY:
+		produce_energy_from_work()
+	elif generationType == GenerationType.ZAB:
+		produce_zabs_from_work()
+	else:
+		assert(false, "WORK ISN'T WORKING IN THIS ENERGY/ZAB THING")
 
 func update_plot_hud_development_specific() -> void:
 	pass
+
+func update_monitor_visual() -> void:
+	if generationType == GenerationType.ENERGY:
+		%Energy.hide()
+		%Zab.show()
+	else:
+		%Energy.show()
+		%Zab.hide()
+
+func update_switch_visual() -> void:
+	# 35 degree alt
+	if generationType == GenerationType.ENERGY:
+		var energyToggleTween: Tween = get_tree().create_tween()
+		energyToggleTween.tween_property(%Switch, "rotation", Vector3(0, deg_to_rad(-180), 0), 0.25).set_trans(Tween.TRANS_CUBIC)
+	elif generationType == GenerationType.ZAB:
+		var zabToggleTween: Tween = get_tree().create_tween()
+		zabToggleTween.tween_property(%Switch, "rotation", Vector3(0, deg_to_rad(-180), deg_to_rad(-35)), 0.25).set_trans(Tween.TRANS_CUBIC)
+		
+
+func toggle_energy_type() -> void:
+	if generationType == GenerationType.ENERGY:
+		generationType = GenerationType.ZAB
+	else:
+		generationType = GenerationType.ENERGY
+	update_monitor_visual()
+	update_switch_visual()
+
+
+func _on_switch_area_mouse_entered() -> void:
+	pass # Replace with function body.
+
+
+func _on_switch_area_mouse_exited() -> void:
+	pass # Replace with function body.
+
+
+func _on_switch_area_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MouseButton.MOUSE_BUTTON_LEFT:
+			toggle_energy_type()
